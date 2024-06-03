60d79f35e38ea27c1632d73afe05423ca9910efb
Fyodor Volkov <keytashi17@gmail.com>
2024-06-03 23:12:15 +0700

    12.25

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-    controllers:[AppController],
-    providers:[AppService]
-})
+    controllers:[],
+    providers:[],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 3306,
+            username: 'posgres',
+            password: 'root',
+            database: 'nest-cors',
+            models: [],
+            autoLoadModels:true
+          }),
+    ],
+    }
+)
 export class AppModule{
 
 }
