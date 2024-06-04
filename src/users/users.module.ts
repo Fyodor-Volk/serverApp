895e1f63c06ed4b4bc5bd7072092465bf95a58d8
Fyodor Volkov <keytashi17@gmail.com>
2024-06-04 23:56:57 +0700

    Add roles full

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,12 +4,19 @@ import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { User } from "./users.model"
 import { Role } from 'src/roles/roles.model';
+import { RolesModule } from 'src/roles/roles.module';
+import {AuthModule} from "../auth/auth.module";
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
-    SequelizeModule.forFeature([User, Role])
-  ]
+    SequelizeModule.forFeature([User, Role, UsersModule]),
+    RolesModule,
+    forwardRef(() => AuthModule),
+  ],
+    exports: [
+        UsersService,
+    ]
 })
 export class UsersModule {}
