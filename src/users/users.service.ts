26fd8039ebde61a1e78513d4aec345f920b13563
Fyodor Volkov <keytashi17@gmail.com>
2024-06-04 21:58:33 +0700

    26:44 create

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import {InjectModel} from "@nestjs/sequelize";
+import {User} from "./users.model";
+import {HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import { CreateUserDto } from "./dto/create-user.dto";
+
 
 @Injectable()
 export class UsersService {
 
-    async createUser(){
+    constructor(@InjectModel(User) private userRepository: typeof User) {}
 
+    async createUser(dto: CreateUserDto){
+        const user = await this.userRepository.create(dto);
+
+        return user;
     }
 
     async getAllUsers(){
+        const users = await this.userRepository.findAll({include: {all: true}});
         
+        return users;
     }
 }
