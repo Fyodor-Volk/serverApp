3908d7b0d273230f9982782b60b41c994de27416
Fyodor Volk <keyashi17@gmail.com>
2024-06-05 10:54:38 +0700

    Add pipes, exception

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Model } from "sequelize-typescript";
+import { BelongsToMany, HasMany, Model } from "sequelize-typescript";
 import { Table, Column, DataType} from "sequelize-typescript";
 import { Col } from "sequelize/types/utils";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles.model";
+import {Post} from "../posts/posts.model";
 
 interface UserCreationAttrs {
     email: string;
@@ -10,20 +13,29 @@ interface UserCreationAttrs {
 
 @Table({tableName: 'users'})
 export class User extends Model<User, UserCreationAttrs >{
-    @ApiProperty({example: '1', description: "Уникальный идентификатор"})
-    @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+    @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
+    @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
 
-    @ApiProperty({example: 'users@mail.ru', description: "Почтовый адрес"})
+    @ApiProperty({example: 'user@mail.ru', description: 'Почтовый адрес'})
     @Column({type: DataType.STRING, unique: true, allowNull: false})
     email: string;
+
     @ApiProperty({example: '12345678', description: 'Пароль'})
     @Column({type: DataType.STRING, allowNull: false})
     password: string;
+
     @ApiProperty({example: 'true', description: 'Забанен или нет'})
     @Column({type: DataType.BOOLEAN, defaultValue: false})
     banned: boolean;
+
     @ApiProperty({example: 'За хулиганство', description: 'Причина блокировки'})
     @Column({type: DataType.STRING, allowNull: true})
     banReason: string;
+
+    @BelongsToMany(() => Role, () => UserRoles)
+    roles: Role[];
+
+    @HasMany(() => Post)
+    posts: Post[];
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,17 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { User } from "./users.model"
-import { Role } from 'src/roles/roles.model';
-import { RolesModule } from 'src/roles/roles.module';
+import {User} from "./users.model";
+import {Role} from "../roles/roles.model";
+import {UserRoles} from "../roles/user-roles.model";
+import {RolesModule} from "../roles/roles.module";
 import {AuthModule} from "../auth/auth.module";
+import {Post} from "../posts/posts.model";
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
   imports: [
-    SequelizeModule.forFeature([User, Role, UsersModule]),
+    SequelizeModule.forFeature([User, Role, UsersModule, Post]),
     RolesModule,
     forwardRef(() => AuthModule),
   ],
